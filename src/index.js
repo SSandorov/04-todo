bd2638433213bf7a627bac8c5b6320e7061860d2
ssandorov <stefansandorovssb@gmail.com>
2022-05-25 14:14:10 +0200

    intorducción al local y session storage

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import {Todo, TodoList} from './classes/index.js'
 import { crearTodoHTML } from './js/componentes.js';
 
 export const listaTareas = new TodoList();
+
+/*
 const tarea = new Todo('Aprender JS!!');
 
 listaTareas.nuevoTodo(tarea);
@@ -18,3 +20,26 @@ listaTareas.nuevoTodo(tarea);
 console.log(listaTareas);
 
 crearTodoHTML(tarea);
+*/
+
+// Trabajamos con el alamacenamiento de la información para poder guardar las tareas
+// aunque cerremos el navegador
+/*
+Hay dos maneras de trabajar con los datos almacenados en la web
+    - localStorage: información que no tiene tiempo de expiración (se guarda en la caché)
+    - sessionStorage: información que sólo se mantiene minetras estemos conectados
+
+Hay que tener en cuenta que para el back-end esto no se aplica, ya que deberemos manejar
+los datos con una base de datos (mongodb, sql)
+*/
+
+// El setItem sólo permite almacenar valores de tipo string
+// localStorage.setItem('mi-llave', 'ABC');
+
+// Para borrar un elemento del localStorage podríamos hacer lo siguiente
+/*
+setTimeout(()=>{
+    localStorage.removeItem('mi-llave');
+}, 1500);
+*/
+// Lo tenemos como referencia por si tenemos que emplearlo alguna vez
